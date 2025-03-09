ceaed69b778f85a27f33f1e08e9568e8e9f2e04c
Rio Mulya Syawal <riomulya75@gmail.com>
2025-03-10 00:56:34 +0700

    update user Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,14 +15,17 @@ class UserSeeder extends Seeder
             'username' => 'admin',
             'official_name' => 'Administrator',
             'official_title' => 'System Admin',
-            'mobile_number' => '08123456789', 
+            'mobile_number' => '08123456789',
             'official_tel' => '0217654321',
             'password' => bcrypt('admin123'),
             'status' => 'A',
             'password_expiry_date' => 0,
             'amend_expired_password' => 'No',
             'created_at' => now(),
-            'updated_at' => now()
+            'updated_at' => now(),
+            'password_expiry_date' => now()->addYear(), // Ganti 0 dengan tanggal valid
+            'amend_expired_password' => 'Yes', // Ganti 'No' jika enum di database mengharuskan 'Yes'
+            'remember_token' => Str::random(10),
         ]);
     }
 }
