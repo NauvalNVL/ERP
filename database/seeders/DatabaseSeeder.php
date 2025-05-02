b634c23169bfc9714722d3794f3f8af86f5a75cf
RasyidJu <abdulrasyid13uu@gmail.com>
2025-05-02 14:49:07 +0700

    update db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SalesTeamSeeder::class,
             SalespersonSeeder::class,
+            SalespersonTeamSeeder::class,
             ProductGroupSeeder::class,
             GeoSeeder::class,
             PaperSizeSeeder::class,
