98501794f37cd36630017b17d48fd23cec975cc0
wazzz23 <fawwaz.fatih23@gmail.com>
2025-06-26 18:03:57 +0700

    vp nonactive customer

--- routes/web.php
+++ routes/web.php
@@ -335,6 +335,10 @@ Route::middleware('auth')->group(function () {
              return Inertia::render('sales-management/system-requirement/customer-account/obsolete-reactive-customer-ac');
          })->name('vue.obsolete-reactive-customer-account.index');
          
+         Route::get('/obsolete-reactive-customer-account/view-print', function () {
+            return Inertia::render('sales-management/system-requirement/customer-account/view-and-print-nonactive-customer');
+        })->name('vue.obsolete-reactive-customer-account.view-print');
+         
          Route::get('/customer-alternate-address', [CustomerAlternateAddressController::class, 'index'])->name('vue.customer-alternate-address.index');
          Route::get('/customer-alternate-address/view-print', function () {
              return Inertia::render('sales-management/system-requirement/customer-account/view-and-print-customer-alternate-address');
