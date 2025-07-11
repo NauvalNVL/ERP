da960105b187247952f5008628ed67dbbf16c001
RasyidJu <abdulrasyid13uu@gmail.com>
2025-07-11 19:01:28 +0700

    update login

--- resources/js/Pages/Auth/login.vue
+++ resources/js/Pages/Auth/login.vue
@@ -31,12 +31,18 @@
                             placeholder="User ID"
                             :disabled="isLoading">
                     </div>
-                    <div>
+                    <div class="relative">
                         <label for="password" class="sr-only">Password</label>
-                        <input id="password" v-model="form.password" type="password" required 
-                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm transition-all duration-300" 
+                        <input id="password" v-model="form.password" :type="showPassword ? 'text' : 'password'" required 
+                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm transition-all duration-300 pr-10" 
                             placeholder="Password"
                             :disabled="isLoading">
+                        <div class="absolute inset-y-0 right-0 pr-3 flex items-center">
+                            <button type="button" @click="showPassword = !showPassword" class="text-gray-400 hover:text-gray-600 focus:outline-none" :disabled="isLoading">
+                                <EyeIcon v-if="!showPassword" class="h-5 w-5" />
+                                <EyeOffIcon v-else class="h-5 w-5" />
+                            </button>
+                        </div>
                     </div>
                 </div>
 
@@ -78,8 +84,10 @@
 import { useForm, router } from '@inertiajs/vue3';
 import { Head } from '@inertiajs/vue3';
 import { ref } from 'vue';
+import { EyeIcon, EyeOffIcon } from '@heroicons/vue/outline';
 
 const isLoading = ref(false);
+const showPassword = ref(false);
 
 const form = useForm({
     user_id: '',
