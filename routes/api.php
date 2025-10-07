49b95e5de30e4ebcc43cb34aee9bb5cb072443fc
RasyidJu <abdulrasyid13uu@gmail.com>
2025-10-07 14:43:05 +0700

    fix route vehicle & vehicle class

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,8 @@ use App\Http\Controllers\DeliveryOrderFormatController;
 use App\Http\Controllers\MaterialManagement\SystemRequirement\MmGlDistributionController;
 use App\Http\Controllers\CustomerWarehouseRequirementController;
 use App\Http\Controllers\UpdateMcController;
+use App\Http\Controllers\VehicleClassController;
+use App\Http\Controllers\VehicleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -666,6 +668,40 @@ Route::prefix('material-management/config-data')->group(function () {
     Route::get('source-codes', [MmConfigController::class, 'getSourceCodes']); // Placeholder, assuming it's simple
     Route::get('gl-distributions', [MmConfigController::class, 'getGlDistributions']); // Placeholder
 });
+
+// Warehouse Management - Delivery Order - Setup - Vehicle Class & Vehicle APIs
+Route::prefix('warehouse-management/delivery-order/setup')->group(function () {
+    // Vehicle Classes
+    Route::get('/vehicle-classes', [VehicleClassController::class, 'apiIndex']);
+    Route::post('/vehicle-classes', [VehicleClassController::class, 'apiStore']);
+    Route::get('/vehicle-classes/{vehicleClass}', [VehicleClassController::class, 'show']);
+    Route::put('/vehicle-classes/{vehicleClass}', [VehicleClassController::class, 'apiUpdate']);
+    Route::delete('/vehicle-classes/{vehicleClass}', [VehicleClassController::class, 'apiDestroy']);
+
+    // Vehicles
+    Route::get('/vehicles', [VehicleController::class, 'apiIndex']);
+    Route::post('/vehicles', [VehicleController::class, 'apiStore']);
+    Route::get('/vehicles/{vehicle}', [VehicleController::class, 'show']);
+    Route::put('/vehicles/{vehicle}', [VehicleController::class, 'apiUpdate']);
+    Route::delete('/vehicles/{vehicle}', [VehicleController::class, 'apiDestroy']);
+
+    // Helpers for dropdowns
+    Route::get('/vehicles/helpers/vehicle-classes', [VehicleController::class, 'getVehicleClasses']);
+    Route::get('/vehicles/helpers/companies', [VehicleController::class, 'getCompanies']);
+});
+
+// Backward-compatible short aliases used by some Vue pages
+Route::get('/vehicles', [VehicleController::class, 'apiIndex']);
+Route::post('/vehicles', [VehicleController::class, 'apiStore']);
+Route::get('/vehicles/{vehicle}', [VehicleController::class, 'show']);
+Route::put('/vehicles/{vehicle}', [VehicleController::class, 'apiUpdate']);
+Route::delete('/vehicles/{vehicle}', [VehicleController::class, 'apiDestroy']);
+
+Route::get('/vehicle-classes', [VehicleClassController::class, 'apiIndex']);
+Route::post('/vehicle-classes', [VehicleClassController::class, 'apiStore']);
+Route::get('/vehicle-classes/{vehicleClass}', [VehicleClassController::class, 'show']);
+Route::put('/vehicle-classes/{vehicleClass}', [VehicleClassController::class, 'apiUpdate']);
+Route::delete('/vehicle-classes/{vehicleClass}', [VehicleClassController::class, 'apiDestroy']);
 
 Route::prefix('delivery-order-formats')->group(function () {
     Route::get('/', [DeliveryOrderFormatController::class, 'getFormatsJson'])->name('delivery-order-formats.index');
